82b77cfac1dd86c0fa2fc045059b228979f44c6b
Samuel P <samuel.p991@gmail.com>
2018-06-07 03:01:34 +0200

    more progress

--- scripts/events/debate.gd
+++ scripts/events/debate.gd
@@ -1,19 +1,28 @@
 extends Node2D
 
+onready var dialog = $dialog
+onready var message1 = $"dialog/message1"
+onready var message2 = $"dialog/message2"
+
 
 func _ready():
+	dialog.hide()
+	remove_child(dialog)
+	$"/root/stage/dialog_layer".add_child(dialog)
+	dialog.show()
+
 	if game.current_max <= 1:
 		return
 	var debate = game.cfg.EXCUSES[game.current_max - 2].debate
-	$message1.set_text(debate.question)
-	$message2.set_text(debate.answer)
+	message1.set_text(debate.question)
+	message2.set_text(debate.answer)
 
 
 func update_text(i):
 	if i <= 1:
-		$message1.set_text("")
-		$message2.set_text("")
+		message1.set_text("")
+		message2.set_text("")
 		return
 	var debate = game.cfg.EXCUSES[i - 2].debate
-	$message1.set_text(debate.question)
-	$message2.set_text(debate.answer)
+	message1.set_text(debate.question)
+	message2.set_text(debate.answer)
