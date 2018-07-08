aa0ff81a7b1897d04d593d2acebbee5050346146
Samuel P <samuel.p991@gmail.com>
2018-07-08 20:44:56 +0200

    on animation finished

--- scripts/popups/debate_screen.gd
+++ scripts/popups/debate_screen.gd
@@ -28,3 +28,8 @@ func close():
 func _on_go_back_pressed():
 	game.sounds.play_audio("click")
 	game.popup_layer.close()
+
+
+func _on_animation_animation_finished(anim_name):
+	if anim_name != "open_debate":
+		return
