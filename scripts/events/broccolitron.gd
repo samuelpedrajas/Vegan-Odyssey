3290694a2cf391449a0fab1f1004923e30f7e737
Samuel P <samuel.p991@gmail.com>
2018-11-07 21:05:54 +0100

    broccolitron progress

--- scripts/events/broccolitron.gd
+++ scripts/events/broccolitron.gd
@@ -8,6 +8,10 @@ var broccolitron_ready = false
 onready var slot1 = $broccolitron/texture/slots/slot1
 onready var slot2 = $broccolitron/texture/slots/slot2
 onready var slot3 = $broccolitron/texture/slots/slot3
+onready var slots = [slot1, slot2, slot3]
+
+var results = []
+var current_slot = 0
 
 
 func start():
@@ -20,21 +24,30 @@ func stop():
 
 func _on_stop_pressed():
 	if broccolitron_ready:
-		game.sounds.play_audio("click")
-		$"/root".set_disable_input(true)
+		#game.sounds.play_audio("click")
+		var res = slots[current_slot].stop()
+		results.append(res)
+		current_slot += 1
+
+	if current_slot > 2:
+		#game.event_layer.stop("broccolitron")
+		broccolitron_ready = true
 
 
 func start_rolling():
-	for slot in [slot1, slot2, slot3]:
+	for slot in slots:
 		$timer.start()
 		yield($timer, "timeout")
 		slot.start()
 
+	$timer.start()
+	yield($timer, "timeout")
+	broccolitron_ready = true
+	$"/root".set_disable_input(false)
+
 
 func _on_anim_animation_finished(anim_name):
-	if anim_name == "appear":
-		#broccolitron_ready = true
-		#$"/root".set_disable_input(false)
+	if anim_name == "appear":	
 		start_rolling()
 	elif anim_name == "disappear":
 		queue_free()
--- scripts/other/slot.gd
+++ scripts/other/slot.gd
@@ -6,11 +6,18 @@ func start():
 	$anim.play("roll")
 
 
+func stop():
+	$anim.stop()
+	return current_number
+
+
 func set_one():
 	current_number = 1
 
+
 func set_two():
 	current_number = 2
+
 
 func set_three():
 	current_number = 3
