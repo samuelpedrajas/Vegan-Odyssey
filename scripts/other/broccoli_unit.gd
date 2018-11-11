c0b6ab1f312202e1c1c568de810b0edad0b2a846
Samuel P <samuel.p991@gmail.com>
2018-11-11 19:53:22 +0100

    particles in broccoli

--- scripts/other/broccoli_unit.gd
+++ scripts/other/broccoli_unit.gd
@@ -1,9 +1,21 @@
-extends Sprite
+extends Node2D
 
 
 var anim_time
 var curve
 var dist
+
+var particle_counter = 0
+var n_particles = 20
+
+
+var sparkle_scene = preload("res://scenes/other/sparkle.tscn")
+var last_sparkle = null
+
+var last_sparkle_removed = false
+
+
+onready var _texture = $texture
 
 
 func _ready():
@@ -13,15 +25,42 @@ func _ready():
 var acc_delta = 0.0
 
 
+func last_sparkle_finished():
+	last_sparkle_removed = true
+
+
+func set_particle():
+	if particle_counter > n_particles:
+		return
+
+	var tex_size = _texture.texture.get_size()
+	var dest_x = randf() * tex_size.x
+	var dest_y = randf() * tex_size.y
+	var pos = _texture.get_position() - tex_size / 2.0
+	var sparkle = sparkle_scene.instance()
+	sparkle.set_position(pos + Vector2(dest_x, dest_y))
+	add_child(sparkle)
+	if particle_counter == n_particles:
+			last_sparkle = sparkle
+			sparkle.connect("tree_exited", self, "last_sparkle_finished")
+	particle_counter += 1
+
+
 func _process(delta):
+	if anim_time / n_particles * particle_counter < acc_delta:
+		set_particle()
+
 	acc_delta += delta / anim_time
 
 	if acc_delta > 1.0:
 		set_process(false)
+		_texture.hide()
+		if not last_sparkle_removed:
+			yield(last_sparkle, "tree_exited")
 		print("Destroying broccoli missile")
 		queue_free()
 	else:
-		set_position(curve.interpolate(0, acc_delta))
+		_texture.set_position(curve.interpolate(0, acc_delta))
 
 
 func get_out_control(origin, dest, intensity):
@@ -32,7 +71,7 @@ func get_out_control(origin, dest, intensity):
 
 
 func start(origin, dest, intensity, anim_time):
-	set_position(origin)
+	_texture.set_position(origin)
 
 	$anim.play("appear")
 
